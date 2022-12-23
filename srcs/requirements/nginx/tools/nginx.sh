d8d2f8ec4e03a2aa1b672fe39d5092d3ca86d5c2
Younes Ismaili <yismaili@e2r11p2.1337.ma>
2022-12-23 22:36:40 +0100

    finish nginx dockerfile

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -3,3 +3,4 @@
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
   openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=wordpress/CN=crendeha.42.fr"
 fi
+exec "$@"
