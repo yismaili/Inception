17b57d0aee12448ce2cf423995096c4e97b4965a
Younes Ismaili <yismaili@e2r6p5.1337.ma>
2022-12-28 21:32:44 +0100

    configuration of nginx

--- srcs/requirements/nginx/tools/nginx_ssl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=wordpress/CN=crendeha.42.fr";
-    echo "SSL certificate Successfully!"
-else
-    echo "SSL certificate found!"
-fi
