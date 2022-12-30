5b12d13d07107a82c954d4b2f324771e08926f73
Younes Ismaili <yismaili@e2r6p5.1337.ma>
2022-12-30 21:07:10 +0100

    configuration of wordpress

--- srcs/requirements/WordPress/tools/conf-db.sh
+++
@@ -1,6 +0,0 @@
- #!/bin/bash
- 
- sed -i "s/define( 'DB_NAME', 'database_name_here' );/define('DB_NAME', 'wp_db');/g" "/var/www/html/wp-config-sample.php"
- sed -i "s/define( 'DB_USER', 'username_here' );/define('DB_USER', 'yismaili');/g" "/var/www/html/wp-config-sample.php"
- sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define('DB_PASSWORD', '1234');/g" "/var/www/html/wp-config-sample.php"
- sed -i "s/define( 'DB_HOST', 'localhost' );/define('DB_HOST', 'mariadb');/g" "/var/www/html/wp-config-sample.php"
--- srcs/requirements/WordPress/tools/wordpress.sh
+++ srcs/requirements/WordPress/tools/wordpress.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 	chown -R www-data:www-data /var/www/*;
 	chown -R 755 /var/www/*;
 	mkdir -p /run/php/;
@@ -11,13 +12,15 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	mv wp-cli.phar /usr/local/bin/wp;
 	cd /var/www/html;
 	wp core download --allow-root;
-	mv /var/www/wp-config.php /var/www/html/
-	wp core install --allow-root --url='yismaili@42.fr' --title='wordpress' --admin_user='error' --admin_password='error1234' --admin_email='error@42.fr'
-	wp user create --allow-root 'yismaili' 'yismaili@42.fr' --user_pass='1234';
+touch /var/www/html/wp-config.php;
+  cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php;
+ sed -i "s/database_name_here/wp_db/g" "/var/www/html/wp-config.php"
+ sed -i "s/username_here/yismaili/g" "/var/www/html/wp-config.php"
+ sed -i "s/password_here/1234/g" "/var/www/html/wp-config.php"
+ sed -i "s/localhost/mariadb/g" "/var/www/html/wp-config.php"
+	#  mv /var/www/wp-config.php /var/www/html/
+	wp core install --allow-root --url='yismaili@42.fr' --title='wordpress' --admin_user='error' --admin_password='error1234' --admin_email='error1337@42.fr'
+	wp user create --allow-root ${USER} ${USER_EMAIL} --user_pass= ${USER_PSSWRD};
 	echo "Wordpress: set up!"
 fi
- sed -i "s/define( 'DB_NAME', 'database_name_here' );/define('DB_NAME', 'wp_db');/g" "/var/www/html/wp-config-sample.php"
- sed -i "s/define( 'DB_USER', 'username_here' );/define('DB_USER', 'yismaili');/g" "/var/www/html/wp-config-sample.php"
- sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define('DB_PASSWORD', '1234');/g" "/var/www/html/wp-config-sample.php"
- sed -i "s/define( 'DB_HOST', 'localhost' );/define('DB_HOST', 'mariadb');/g" "/var/www/html/wp-config-sample.php"
 exec "$@"
