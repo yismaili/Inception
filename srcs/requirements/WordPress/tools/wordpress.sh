8680c9f6844f3e3516c7294068e2ae1e34b22692
Younes Ismaili <yismaili@e2r5p2.1337.ma>
2023-01-02 18:55:40 +0100

    env

--- srcs/requirements/WordPress/tools/wordpress.sh
+++ srcs/requirements/WordPress/tools/wordpress.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
-	chown -R www-data:www-data /var/www/*;
-	chown -R 755 /var/www/*;
 	mkdir -p /run/php/;
 	touch /run/php/php7.3-fpm.pid;
+	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 if [ ! -f /var/www/html/wp-config.php ]; then
 	mkdir -p /var/www/html
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
@@ -12,10 +10,10 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	cd /var/www/html;
 	wp core download --allow-root;
 	touch /var/www/html/wp-config.php;
-	sed -i "s/database_name_here/wp_db/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/username_here/yismaili/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/password_here/1234/g" "/var/www/html/wp-config-sample.php"
-	sed -i "s/localhost/mariadb/g" "/var/www/html/wp-config-sample.php"
+	sed -i "s/database_name_here/${DB_NAME}/g" "/var/www/html/wp-config-sample.php"
+	sed -i "s/username_here/${DB_USER}/g" "/var/www/html/wp-config-sample.php"
+	sed -i "s/password_here/${DB_PASSRD}/g" "/var/www/html/wp-config-sample.php"
+	sed -i "s/localhost/${DB_HOST}/g" "/var/www/html/wp-config-sample.php"
  	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php;
 	wp core install --allow-root --url=${URL} --title=${TITLE} --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PSSWRD} --admin_email=${ADMIN_EMAIL}
 	wp user create --allow-root ${USER} ${USER_EMAIL} --user_pass= ${USER_PASSWRD};
