e2fdb444121cd0e6d24a8367d625e457c003d9d4
Younes Ismaili <yismaili@e2r6p4.1337.ma>
2023-01-03 18:25:44 +0100

    fix creation of database if you need to create a nathar One

--- srcs/requirements/mariadb/tools/add_db.sh
+++ srcs/requirements/mariadb/tools/add_db.sh
@@ -8,10 +8,10 @@ service mysql start
 # mysql  -u root  -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%';"
 # mysql -u root  -e "FLUSH PRIVILEGES;"
 
- mysql -u root -e "CREATE DATABASE $DB_NAME";
- mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" ;
- mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;" ;
- mysql -u root -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" ;
+ mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $DB_NAME";
+ mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" ;
+ mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;" ;
+ mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" ;
 # Create the database
 # Create the user
 # Grant privileges to the user
