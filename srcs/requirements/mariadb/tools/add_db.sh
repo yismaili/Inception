4536e72cf88ea83b17ac9ef1ac07ba1226216008
Younes Ismaili <yismaili@e2r6p2.1337.ma>
2023-01-04 18:13:10 +0100

    finish mandatory part

--- srcs/requirements/mariadb/tools/add_db.sh
+++ srcs/requirements/mariadb/tools/add_db.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
 service mysql start
-# mysql  -u root  -e "CREATE DATABASE ${DB_NAME}";
-
-# mysql  -u root  -e "CREATE USER ${DB_USER}@'%' IDENTIFIED BY '${DB_PSSWRD}'";
-
-# mysql  -u root  -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%';"
-# mysql -u root  -e "FLUSH PRIVILEGES;"
-
- mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $DB_NAME";
- mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" ;
- mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;" ;
- mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" ;
-# Create the database
-# Create the user
-# Grant privileges to the user
-
-# Flush privileges
-
-exec "$@"
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" ;
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;" ;
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" ;
+cd  /var/run/mysqld/
+kill `cat mysqld.pid`
+ exec "$@"
