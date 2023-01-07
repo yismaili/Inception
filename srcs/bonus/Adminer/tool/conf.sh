7955bb60fe3a388938228791806af73e1ae2bf1c
Younes Ismaili <yismaili@e2r6p5.1337.ma>
2023-01-07 22:36:28 +0100

    add simple static website

--- srcs/bonus/Adminer/tool/conf.sh
+++ srcs/bonus/Adminer/tool/conf.sh
@@ -1,7 +1,5 @@
 #!bin/sh
 
+apt install -y wget
 wget "http://www.adminer.org/latest.php" -O /var/www/html/index.php
-# chown -R www-data:www-data /var/www/html/index.php
-# chmod 755 /var/www/html/index.php
-
 exec "$@"
