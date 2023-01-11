b301c86e425e56f0741ce198bd0d8a03f5a8fe09
Younes Ismaili <yismaili@e2r12p4.1337.ma>
2023-01-11 23:04:35 +0100

    add ftp dockerfile and conf of ftp server

--- srcs/bonus/ftp_server/tools/conf.sh
+++ srcs/bonus/ftp_server/tools/conf.sh
@@ -24,3 +24,44 @@ echo "local_root=/home/yismaili/ftp" >> /etc/vsftpd.conf
 echo "yismaili" | tee -a /etc/vsftpd.userlist 
 killall -9 vsftpd
 exec "$@"
+# creates the user's home directory if it does not exist
+
+# creates any missing parent directories in the path
+ 
+
+# removes write permissions for all users 
+  
+
+# changes the ownership of this the directory and the group
+
+#he vsftpd server to listen for incoming connections on all interfaces and the default port 21.
+
+# the vsftpd server to not listen for incoming connections on IPV6 addresses the FTP server will only accept 
+# connections made over IPV4 addresses and will not bind to any IPV6 addresses.
+
+# allow data connections to be made from port 20 , this  data connection is used to transfer files to and from the server during a file transfer
+# and the client opens a new connection to the server from a random port
+
+# not allow anonymous connect to the server, the server will only allow connections from users with a valid account on the system.
+
+#the vsftpd will allow users with accounts on the system to connect and authenticate using their username and password.
+
+# users to upload files to the server and By default, vsftpd only allows users to download files
+
+#the local users will only have access to the files and directories within their home directory 
+ 
+#specific range of ports needs to be opened for passive mode connections to work.
+
+
+#llows to control who is able to login to the ftp server by means of a list of usernames.
+
+#the location of the user list file, as specified by "userlist_file" directive.
+
+ #the token that will be replaced by the user's login name. This directive only applies when the local_root directive is used in the config file.
+
+#allowing only the listed users in the 'userlist_file' directive to login
+
+#set the local root directory for users when they first log in and This is useful 
+#if you want to limit the user's access to specific parts of the file system or for security reasons
+ 
+# add the user in the file (list of users)
