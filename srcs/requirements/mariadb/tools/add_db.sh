a28219a76db943456e84cca2cb859b5975c0a19e
Younes Ismaili <yismaili@e2r12p5.1337.ma>
2023-01-17 00:13:31 +0100

    fix mariadb

--- srcs/requirements/mariadb/tools/add_db.sh
+++ srcs/requirements/mariadb/tools/add_db.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 service mysql start
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" ;
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;" ;
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" ;
-cd  /var/run/mysqld/
-kill `cat mysqld.pid`
- exec "$@"
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PSSWRD'" ;
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;" ;
+mysql -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" ;
+kill `cat /var/run/mysqld/mysqld.pid`
+mysqld
