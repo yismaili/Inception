a2471d31263f64e8817274a847efd43532f4f4e7
Younes Ismaili <yismaili@e2r12p4.1337.ma>
2023-01-17 19:33:21 +0100

    finish

--- srcs/requirements/nginx/tools/ssl.sh
+++ srcs/requirements/nginx/tools/ssl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sed -i "s/etc\/ssl\/certs\/nginx.crt/${certs_crt}/g" "/etc/nginx/sites-enabled/default"
 sed -i "s/etc\/ssl\/private\/nginx.key/${private_key}/g" "/etc/nginx/sites-enabled/default"
+sed -i "s/yismaili.42.fr/${domain_name}/g" "/etc/nginx/sites-enabled/default"
 exec "$@"
